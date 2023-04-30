d088d8d55f7fee62ba7d24c03fb1bc6bd79c6598
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-29 22:22:27 -0300

    Criada classe Cartao com suas propriedades e métodos

---
+++ Projeto/Cartao.cs
@@ -0,0 +1,16 @@
+namespace Projeto
+{
+    public abstract class Cartao
+    {
+        //Propriedades
+        public string Bandeira { get; set; } = string.Empty;
+        public string NumeroCartao { get; set; } = string.Empty;
+        public string Titular { get; set; } = string.Empty;
+        public string Cvv { get; set; } = string.Empty;
+
+        //Métodos
+        public abstract void Pagar();
+        public abstract string SalvarCartao();
+
+    }
+}
